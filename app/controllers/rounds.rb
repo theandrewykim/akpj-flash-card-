8a6e5e803471aaf9de3b4ea053b3434461bf084e
Karen Ball & Joe Plonsker <pair+greensneakers.plonsker@devbootcamp.com>
2016-03-05 16:05:02 -0500

    Rounds are generated properly

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
 get '/decks/:id/rounds' do
-  "hello world"
 
   @deck = Deck.find_by(id: params[:id])
   @card = @deck.cards.sample
+  User.find(session[:logged_in]).rounds << Round.create(deck_id: @deck.id)
+binding.pry
 
   erb :'/rounds/show'
 end
