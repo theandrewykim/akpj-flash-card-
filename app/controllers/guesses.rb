58453af4b6fae967cb02e76e0defc1b84b7cbf60
Karen Ball & Joe Plonsker <pair+greensneakers.plonsker@devbootcamp.com>
2016-03-05 15:42:39 -0500

    Users can signup/login/logot

--- app/controllers/guesses.rb
+++ app/controllers/guesses.rb
@@ -1,33 +1,19 @@
 
 post '/decks/cards/:id/rounds' do
   @answered_card = Card.find(params[:id])
-  User.create(username:"Asian Peter", password_hash:"GreekPeter")
-  Round.create(user_id:1, deck_id:1)
-
-def pick_card(card)
-    if card.correct_yet?
-      pick_card(card.deck.cards.sample)
-   else
-     @card = card
-  end
-end
+  # User.create(username:"Asian Peter", password_hash:"GreekPeter")
+  # Round.create(user_id:1, deck_id:1)
+  @deck = @answered_card.deck
 
 
 if @answered_card.answer == params[:answer][:answer]
   @answered_card.guesses << Guess.create(round_id: 1, correct?: true)
-  if  !@answered_card.deck.all_correct?
-    random_card = @answered_card.deck.cards.sample
-    pick_card(random_card)
-
+   @card = @deck.pick_cards_until_over
     erb :'/rounds/show'
-  end
-  else
+else
    @answered_card.guesses << Guess.create(round_id: 1, correct?: false)
-    if  !@answered_card.deck.all_correct?
-      random_card = @answered_card.deck.cards.sample
-    pick_card(random_card)
+       @card = @deck.pick_cards_until_over
     erb :'/rounds/show'
-    end
   end
 
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -6,4 +6,20 @@ class Deck < ActiveRecord::Base
     self.cards.all?{|card| card.correct_yet? == true}
   end
 
+  def pick_card(card)
+    if card.correct_yet?
+      pick_card(card.deck.cards.sample)
+   else
+      card
+    end
+  end
+
+def pick_cards_until_over
+    if  !self.all_correct?
+    random_card = self.cards.sample
+    self.pick_card(random_card)
+  end
+end
+
+
 end
